262bfc7e8667faeaed0e0512bae09a132264cb32
Wai Hyn Htun <=waihynhtun1994@gmail.com>
2023-03-19 01:00:56 +0700

    Resource API initial commit

---
+++ app/Models/Lottery/Number.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Lottery;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Number extends Model
+{
+    use HasFactory;
+}
